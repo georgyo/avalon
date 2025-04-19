ed653540def7367323b56599758f65e3d26101d2
George Shammas <george@shamm.as>
2025-04-19 06:28:31 -0400

    swtich avalonlib to mjs

--- server/avalon-server.js
+++ server/avalon-server.js
@@ -1,6 +1,6 @@
 const firebaseAdmin = require('firebase-admin');
 const _ = require('lodash');
-const avalonLib = require('./common/avalonlib');
+const avalonLib = require('./common/avalonlib.mjs');
 
 const db = firebaseAdmin.firestore();
 const FieldValue = firebaseAdmin.firestore.FieldValue;
